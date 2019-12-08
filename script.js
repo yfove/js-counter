c4fd684da589d5626adaf381f741e1ccb17a75a8
curtis chat <curtis.ch.chat@gmail.com>
2019-12-08 01:42:02 -0700

    counter subtract on click and set goal

--- script.js
+++ script.js
@@ -8,5 +8,30 @@ $(document).ready(() => {
 		counter = counter + 1;
 		$('.counter').text(counter);
 		checkGoal(counter);
+    });
+    
+    // when user clicks on the subtract button, subtract a number from the counter
+	// link the counter variable to the counter HTML
+	$('#subtract').on('click', () => {
+		counter = counter - 1;
+		$('.counter').text(counter);
+		checkGoal(counter);
 	});
+
+	let goal;
+
+	$('form').on('submit', (e) => {
+		e.preventDefault();
+		goal = parseInt($('input#goal').val());
+		$('input[type=submit]').addClass('submitted');
+		$('input[type=text]').addClass('set');
+	});
+
+	const checkGoal = (current) => {
+		if (current === goal) {
+			alert(`You met your goal of ${goal}`);
+			$('input[type=submit]').removeClass('submitted');
+			$('input[type=text]').removeClass('set').val('');
+		}
+	}
 });
